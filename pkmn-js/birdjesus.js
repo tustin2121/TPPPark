eb41d24004ec7ebaa1c1fde3b501f286cd17743e
Tim Pittman <tustin2121@gmail.com>
2014-05-01 17:05:02 -0400

    First Event
    - Bird Jesus and the Church of Helix (to be split into two)

---
+++ pkmn-js/birdjesus.js
@@ -0,0 +1,14 @@
+// 
+// Bird Jesus and his Church of Helix
+//
+
+addEvent(new Building({
+	name: "The Church of Helix",
+	img: "img/church.png",
+	x : 8, y : 8,
+}));
+
+// addEvent(new Pokemon({
+// 	name: "Bird Jesus",
+// 	x : 0, y : 0,
+// }));
