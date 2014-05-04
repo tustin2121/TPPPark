061c24275496f482704e286116190d13f0535234
Tim P <tustin2121@gmail.com>
2014-05-04 15:57:46 -0400

    Much Progress
    - Redid the map of the park quite a bit
    - Added Summary Screen
    - Added all of our Red hall of fame pokemon (as events, though they're
    not placed yet)
    - Added Multi-Event, but something is wrong where only the last event in
    the map is triggering
    - Made Battle of Mt Silver a Multi-Event

--- pkmn-js/gen1.js
+++ pkmn-js/gen1.js
@@ -3,27 +3,172 @@
 // 
 
 // Bird Jesus and his Church of Helix
-
 addEvent(new Building({
 	name: "The Church of Helix",
-	img: "img/pkmn/church.png",
+	img: "img/bld/church.png",
 	x : 8, y : 8,
 }));
 
 addEvent(new Pokemon({
 	name: "Bird Jesus",
-	OT: "Red",
 	floating: true,
 	img: "img/pkmn/bird-jesus.png",
 	shadow : "img/pkmn/bird-jesus-shadow.png",
 	x : 10, y : 8, z: 20,
+	
+	dex : "img/pkdx/tpp1_birdjesus.gif",
+	dexsource : "http://hamigakimomo.tumblr.com/post/77911455524/i-decided-to-make-custom-r-b-sprites-for-the",
+	
+	OT: "Red",
+	gamename: "aaabaaaajss",
+	pokename: "Pidgeot",
+	level: 69,
+	nicknames : "Abba Jesus",
+	
+	ribbons : [
+		new HallOfFame_Ribbon("16d 7h 45m"),
+	],
+}));
+
+//
+// Note: Lord Helix is attached to the Battle of Mt Silver multi-event defined
+// in the Gen 2 file. See there for his event definition.
+//
+
+// ATV
+addEvent(new Pokemon({
+	name : "ATV",
+	
+	dex : "img/pkdx/tpp2_atv.gif",
+	dexsource : "http://hamigakimomo.tumblr.com/post/77911455524/i-decided-to-make-custom-r-b-sprites-for-the",
+	
+	OT: "Red",
+	gamename : "AATTVVV",
+	pokename : "Venomoth",
+	level : 39,
+	nicknames : "All-Terrain Venomoth<br/>The Dragonslayer",
+	memo : "Once took down Lance's Dragonite singlehandedly.",
+	
+	ribbons : [
+		new HallOfFame_Ribbon("16d 7h 45m"),
+	],	
+}));
+
+//The Fonz
+addEvent(new Pokemon({
+	name : "King Fonz",
+	
+	dex : "img/pkdx/tpp3_fonz.gif",
+	dexsource : "http://hamigakimomo.tumblr.com/post/77911455524/i-decided-to-make-custom-r-b-sprites-for-the",
+	
+	OT: "Red",
+	gamename : "AAAAAAAAAA",
+	pokename : "Nidoking",
+	level : 54,
+	memo : "Survived and entered party on Bloody Sunday",
+	caught : "Caught in Safari Zone",
+	ball : "safari",
+	
+	ribbons : [
+		new HallOfFame_Ribbon("16d 7h 45m"),
+	],	
+}));
+
+//Air
+addEvent(new Pokemon({
+	name : "Air",
+	
+	dex : "img/pkdx/tpp6_air.gif",
+	dexsource : "http://hamigakimomo.tumblr.com/post/77911455524/i-decided-to-make-custom-r-b-sprites-for-the",
+	
+	OT: "Red",
+	gamename : "AIIIIIIRRR",
+	pokename : "Lapras",
+	nicknames : "Air Jordan<br/>Fresh Prince of Bel AIR",
+	level : 31,
+	
+	ribbons : [
+		new HallOfFame_Ribbon("16d 7h 45m"),
+	],	
+}));
+
+// Zapdos
+addEvent(new Pokemon({
+	name : "Battery Jesus",
+	
+	dex : "img/pkdx/tpp5_zapdos.gif",
+	dexsource : "http://hamigakimomo.tumblr.com/post/77911455524/i-decided-to-make-custom-r-b-sprites-for-the",
+	
+	OT: "Red",
+	gamename : "AA-j",
+	pokename : "Zapdos",
+	nicknames : "Double-A Jay<br/>Archangel of Justice",
+	level : 81,
+	caught : "With Masterball 10d 7h 49m",
+	ball : "master",
+	memo : "Attempts at retreival caused Bloody Sunday. Invaluable at Elite Four.",
+	
+	ribbons : [
+		new HallOfFame_Ribbon("16d 7h 45m"),
+	],	
 }));
 
 // Red's Abby
-
 addEvent(new Pokemon({
 	name: "Abby",
-	OT: "Red",
 	img: "img/pkmn/abby-gen1.png",
 	x : -14, y : -34,
+	
+	dex : "img/pkdx/tpp9_abby.gif",
+	dexsource : "http://hamigakimomo.tumblr.com/post/78067393358/ive-updated-omanytes-sprite-to-omastar-i-also",
+	
+	OT: "Red",
+	gamename : "ABBBBBBK",
+	pokename : "Charmelion",
+	level : 34,
+	ribbons : [
+		new Starter_Ribbon(),
+		new Released_Ribbon("4d 8h 53m"),
+	],
+}));
+
+// DigRat
+addEvent(new Pokemon({
+	name: "DigRat",
+	
+	dex : "img/pkdx/tpp11_digrat.gif",
+	dexsource : "http://hamigakimomo.tumblr.com/post/78067393358/ive-updated-omanytes-sprite-to-omastar-i-also",
+	
+	OT: "Red",
+	gamename : "AAJST(???",
+	pokename : "Raticate",
+	nicknames : "BigDig",
+	level : 20,
+	memo : "A trouble maker, known for digging us out of places we wanted to be.",
+	
+	ribbons : [
+		new Released_Ribbon("10d 16h 2m"),
+		new BloodySunday_Ribbon(),
+	],
+}));
+
+// DUX!
+addEvent(new Pokemon({
+	name : "DUX",
+	
+	dex : "img/pkdx/tpp10_dux.gif",
+	dexsource : "http://hamigakimomo.tumblr.com/post/78067393358/ive-updated-omanytes-sprite-to-omastar-i-also",
+	
+	OT: "Red",
+	gamename : "DUX",
+	pokename : "Farfetch'd",
+	level : 31,
+	nicknames : '"The Slayer of Trees"',
+	memo : "Known for its mighty Cut ability!",
+	
+	ribbons : [
+		new Traded_Ribbon("For Spearow 2d 6h 59m"),
+		new Released_Ribbon("10d 13h 13m"),
+		new BloodySunday_Ribbon(),
+	],
 }));
--- pkmn-js/gen2.js
+++ pkmn-js/gen2.js
@@ -1,7 +1,7 @@
 // gen2.js
 // File for TPP Crystal events
 // 
-
+/*
 addEvent(new Pokemon({
 	name: "Battle of Mt Silver",
 	OT: "Red",
@@ -9,3 +9,54 @@ addEvent(new Pokemon({
 	breathe : false,
 	x : -4, y : -16, z:-6,
 }));
+*/
+
+addEvent(new MultiEvent({
+	name : "Battle of Mt Silver",
+	img: "img/pkmn/MtSilverBattle.gif",
+	x : -4, y : -16, z:-6,
+}).addSubEvent("47,17,75,40", new Pokemon({
+	name : "Lord Helix",
+	
+	dex : "img/pkdx/tpp8_omastar.gif",
+	dexsource : "http://hamigakimomo.tumblr.com/post/78067393358/ive-updated-omanytes-sprite-to-omastar-i-also",
+	
+	OT: "Red",
+	gamename : "Omastar",
+	pokename : "Omastar",
+	level : 52,
+	memo : "God of the Religon of TPP.",
+	caught : "Revived at 11d 10h 35m",
+	
+	ribbons : [
+		new HallOfFame_Ribbon("16d 7h 45m"),
+	],	
+})).addSubEvent("2,19,33,40", new Pokemon({
+	name : "LazorGator",
+	
+	OT: "AJDNNW",
+	gamename : "AAAAAtttta",
+	pokename : "Feraligatr",
+	level : 84,
+	nicknames : "General Gator",
+	memo : "Knew Leer early on, which looked like laser beams.",
+	
+	ribbons : [
+		new Starter_Ribbon(),
+		new HallOfFame_Ribbon("9d 21h 24m"),
+	],	
+})).addSubEvent("13,3,28,19", new Pokemon({
+	name : "Omelette",
+	
+	OT: "AJDNNW",
+	gamename : "Togepi",
+	pokename : "Togepi",
+	level : 12,
+	nicknames : "Prince Omelette",
+	memo : "Once used Sacred Fire via Metronome.",
+	
+	ribbons : [
+		new Released_Ribbon("3d 15h 24m"),
+	],	
+}))
+);
