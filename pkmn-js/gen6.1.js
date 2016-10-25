500cc85b5395568b0066bfb622a4369b554fbbad
tustin2121 <tustin2121@gmail.com>
2016-10-24 22:09:50 -0400

    Finally changing the date on this.
    We're coming up to 1000 days of TPP as I make this change.

--- pkmn-js/gen6.1.js
+++ pkmn-js/gen6.1.js
@@ -3,10 +3,10 @@
 //
 
 (function(){
-	var date = new Date(Date.UTC(2014, 11-1, 22, 0+3));
-	if (date.getTime() > new Date().getTime()) {
-		showTimer("TPP ORAS starts in<br/>", date);
-	} else {
-		showGameClock("TPP ORAS Game Clock<br/>", date);
-	}
+	var date = new Date(Date.UTC(2014, 2-1, 12, 23, 16));
+//	if (date.getTime() > new Date().getTime()) {
+//		showTimer("TPP ORAS starts in<br/>", date);
+//	} else {
+		showGameClock("Time since TPP began<br/>", date);
+//	}
 })(); 
